784a2c8d42641733f8b26072c051232fe2899636
jhchoi212 <86747222+jhchoi212@users.noreply.github.com>
2022-02-08 12:15:39 -0800

    root-opener

---
+++ open_root.py
@@ -0,0 +1,11 @@
+import ROOT
+import numpy as np
+
+inFileName = sys.argv[1]
+print("Reading from " + str(inFileName))
+
+inFile = ROOT.TFile.Open(inFileName,"READ")
+
+tree = inFile.Get("ntuple0/objects")
+ver = inFile.Get("ntuple0/objects/vz")
+tree.show()
